d06018a3b60cf7aadd63dc9b8657c24bebf77fbb
enriquepalaciosb <enriquegpalacios@gmail.com>
2022-01-15 15:32:17 -0800

    Added all files and working on JS

---
+++ main.js
@@ -0,0 +1,16 @@
+let usersWords = []; 
+var ulItems = document.getElementById("listOfWords");
+
+function insertWords() {
+    for (let i = 0; i < 3; i++) {
+        let usersResponse = prompt("Enter a word, any word");
+        var usersWords[i]= usersResponse;
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode)
+    }
+};
+function swapWords() {
+    oneWord.charAt(oneWord.length - 1) + oneWord.substring(1, oneWord.length - 1) + oneWord.charAt(0); });
+};
+
+
