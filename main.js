943e02f6900002878d17b9b07d73643ac6b6ca1a
enriquepalaciosb <enriquegpalacios@gmail.com>
2022-01-15 16:27:23 -0800

    Making progress on JS

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ function insertWords() {
     for (let i = 0; i < 3; i++) {
         let usersResponse = prompt("Enter a word, any word");
         var usersWords[i]= usersResponse;
-        var li = document.createElement('li');
-        li.appendChild(document.createTextNode)
     }
 };
+//when it runs, pull info from array
+function getWords {
+    
+}
 function swapWords() {
     oneWord.charAt(oneWord.length - 1) + oneWord.substring(1, oneWord.length - 1) + oneWord.charAt(0); });
 };
