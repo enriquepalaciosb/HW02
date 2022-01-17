f7021f9ad53cdf239d504850c81c73287357fce7
epearl-tv <97500879+ianol34@users.noreply.github.com>
2022-01-16 23:18:47 -0800

    adding swap text function and reskinning section 2

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let usersWords = []; 
 var ulItems = document.getElementById("listOfWords");
+var ulSwappedItems = document.getElementById("listOfSwappedWords");
 
 function insertWords() {
     for (let i = 0; i < 3; i++) {
@@ -10,9 +11,17 @@ function insertWords() {
         let listItem = ulItems.appendChild(document.createElement("li"));
         listItem.innerHTML= element; 
     });
+	document.getElementById("startButton").style.display = "none";
+	document.getElementById("swapButton").style.display = "block";
 };
-// function swapWords() {
-//     oneWord.charAt(oneWord.length - 1) + oneWord.substring(1, oneWord.length - 1) + oneWord.charAt(0); });
-// };
+function swapWords() {
+	document.getElementById("firstSection").style.display = "none";	
+	document.getElementById("secondSection").style.display = "block";	
+	const map1 = usersWords.map(x => x.charAt(x.length - 1) + x.substring(1, x.length - 1) + x.charAt(0)); 
+	map1.forEach(element => {
+        let listItem = ulSwappedItems.appendChild(document.createElement("li"));
+        listItem.innerHTML= element; 
+    });
+};
 
 
